4f4cb262205221e12ecbf6d1062f0f5b16962e47
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-03-28 20:45:05 -0600

    Added path and key upload fields

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module simple-cdn
 
-go 1.21.0
+go 1.22.1
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
+// -------------- Globals --------------
+var uploadKey string
+
+// -------------- Main --------------
 func main() {
 	ip := os.Getenv("IP_ADDRESS")
 	if ip == "" {
@@ -15,14 +20,73 @@ func main() {
 	if port == "" {
 		port = "3004"
 	}
+	uploadKey = os.Getenv("UPLOAD_KEY")
 
 	router := http.NewServeMux()
 
 	router.Handle("/", http.FileServer(http.Dir("./static")))
+	router.HandleFunc("GET /upload", uploadPageHandler)
+	router.HandleFunc("POST /upload", uploadHandler)
 
 	server := http.Server{
 		Addr:    ip + ":" + port,
 		Handler: router,
 	}
 	log.Fatal(server.ListenAndServe())
+}
+
+// -------------- Handlers --------------
+// Upload page handler
+func uploadPageHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Upload</title>
+		</head>
+		<body>
+			<form action="/upload" method="post" enctype="multipart/form-data">
+				<input type="text" name="upload_key" required/>
+				<input type="text" name="upload_path" />
+				<input type="file" name="file" required/>
+				<input type="submit" value="Upload" />
+			</form>
+		</body>
+	</html>
+	`
+	w.Write([]byte(html))
+}
+
+// Upload handler
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("upload_key") != uploadKey {
+		http.Error(w, "Invalid upload key", http.StatusUnauthorized)
+		return
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	path := r.FormValue("upload_path")
+	for len(path) > 0 && path[0] == '/' {
+		path = path[1:]
+	}
+
+	dst, err := os.Create("/" + path + "/" + handler.Filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("File uploaded successfully"))
 }
